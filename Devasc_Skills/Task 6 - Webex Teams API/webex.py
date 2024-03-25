6b74eb796c84e7c9701672e42c7346160d07cf46
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-03-25 07:05:44 +0100

    Webex-start

---
+++ Devasc_Skills/Task 6 - Webex Teams API/webex.py
@@ -0,0 +1,10 @@
+import requests
+from urllib.parse import urlencode
+
+client_id = 'C9b130d70cb6ed40c677d4070378b7eeb9cf85d93060de2d6c67dd763b43d81bc'
+client_secret = 'd2c4dbee48e3692ef661939798a02e0720b90568ba9411cb2b79b1cb95ca0670'
+redirect_uri = 'http://localhost:3000/oauth/callback'
+authorization_code = 'AUTHORIZATION_CODE'
+space_name = 'devasc_skills_your_MAGS@Mercantec'
+members = ['mags@mercantec.dk','yvan.rooseleer@biasc.be']
+github_repo_url = 'https://github.com/MAGS-GH/Devasc_Skills_MAGSMercantec'
